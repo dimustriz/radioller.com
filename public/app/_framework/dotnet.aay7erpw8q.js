5910752c69f00372d1923ed6f3e5fbb41355e40e
Dmitry Korolev <kdmitry@deloittee.com>
2026-08-02 09:32:50 +0200

    Updated app

--- public/app/_framework/dotnet.aay7erpw8q.js
+++ public/app/_framework/dotnet.aay7erpw8q.js
@@ -296,7 +296,8 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "debugLevel": 0,
   "linkerEnabled": true,
   "appsettings": [
-    "../appsettings.Development.json"
+    "../appsettings.Development.json",
+    "../appsettings.json"
   ],
   "globalizationMode": "sharded",
   "extensions": {
